8375a13131a4fab333fef03a544e4eb43ad50ad6
HARSHIT <harshitkr9435@gmail.com>
2024-06-16 09:43:58 +0530

    pb-4

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
             </div>
             <p className="text-xl mt-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi fugiat asperiores, voluptatum, deserunt explicabo reiciendis itaque quod culpa corporis dolorem laudantium illo consequuntur nesciunt saepe quasi sunt voluptatibus similique tempora beatae cupiditate maiores accusantium. Natus distinctio eaque nam commodi reprehenderit accusamus aspernatur reiciendis illo necessitatibus eum, error molestiae dicta atque.</p>
             <br />
-            <p className="text-xl">
+            <p className="text-xl pb-4">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur accusantium et non, iusto cumque quia nam tenetur illum exercitationem distinctio unde facere voluptates odio rem neque maxime ullam, asperiores fuga enim laborum beatae 
             </p>
         </div>
