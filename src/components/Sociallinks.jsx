3d7cb0f29689111267d9f9d4542e8b5ba44ad6c8
HARSHIT <harshitkr9435@gmail.com>
2024-06-28 12:59:05 +0530

    resume updated

--- src/components/Sociallinks.jsx
+++ src/components/Sociallinks.jsx
@@ -39,7 +39,7 @@ const Sociallinks = () => {
               Resume <BsFillPersonLinesFill size={30} />
             </>
           ),
-          href: "/resume.pdf",
+          href: "/Harshit_Resume.pdf",
           style: "rounded-br-md",
           download: true,
         },
